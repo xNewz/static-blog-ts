3b29ea29e3b1fabc416bc535ee923e78e260ee0d
xNewz <pargornruasijan@gmail.com>
2024-05-23 16:41:44 +0700

    add: site header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Pargorn.Ru",
+  url: "https://xnewz.site",
+  description: "Pargorn.Ru",
+  author: "Pargorn Ruasijan",
+  links: {
+    github: "https://github.com/xnewz",
+    twitter: "https://twitter.com/xnewz",
+    personalSite: "https://xnewz.site",
+  },
+};
+
+export type siteConfig = typeof siteConfig;
