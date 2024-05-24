a03609326a824a26288e1488cf15e4daf87fe0d5
xNewz <pargornruasijan@gmail.com>
2024-05-24 15:15:59 +0700

    add: styles header tags

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -10,8 +10,12 @@ const useMDXComponent = (code: string) => {
 const components = {
   Image,
   Callout,
-  p: (props: any) => <p {...props} className="text-lg leading-relaxed" />,
-  ul: (props: any) => <ul {...props} className="text-lg leading-relaxed" />,
+  p: (props: any) => <p {...props} className="text-sm md:text-lg leading-relaxed" />,
+  ul: (props: any) => <ul {...props} className="text-sm md:text-lg leading-relaxed" />,
+  h1: (props: any) => <h1 {...props} className="text-2xl md:text-4xl font-bold mt-8 mb-4" />,
+  h2: (props: any) => <h2 {...props} className="text-xl md:text-3xl font-bold mt-8 mb-4" />,
+  h3: (props: any) => <h3 {...props} className="text-lg md:text-2xl font-bold mt-8 mb-4" />,
+  h4: (props: any) => <h4 {...props} className="text-base md:text-xl font-bold mt-8 mb-4" />,
 };
 
 interface MdxProps {
