fca25cf6d7abaae35e8de819c841a9bfcdaef439
xNewz <pargornruasijan@gmail.com>
2024-05-24 21:00:05 +0700

    fixed: max width

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -37,7 +37,7 @@ export default function TagPage({ params }: TagPageProps) {
   const sortedTags = sortTagsByCount(tags);
 
   return (
-    <div className="container max-w-4xl py-6 lg:py-10">
+    <div className="mx-auto max-w-5xl px-6 py-12">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-black text-4xl lg:text-5xl capitalize">
