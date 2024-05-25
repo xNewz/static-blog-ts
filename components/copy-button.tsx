c82b7c9a461ab8f6397900befb6cc0a7edfb915d
xNewz <pargornruasijan@gmail.com>
2024-05-25 16:47:08 +0700

    add: copy to clipboard

---
+++ components/copy-button.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { useState } from "react";
+import clsx from "clsx";
+import { ClipboardIcon, CheckIcon } from "lucide-react";
+
+const buttonClasses =
+  "flex items-center justify-center px-2 py-1 text-xs font-medium text-white bg-zinc-500 rounded-md hover:bg-zinc-600 focus:outline-none focus-visible:ring focus-visible:ring-zinc-500 focus-visible:ring-opacity-75";
+
+interface CopyButtonProps {
+  text: string;
+  className?: string;
+}
+
+export function CopyButton({ text, className }: CopyButtonProps): JSX.Element {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const copy = async (): Promise<void> => {
+    await navigator.clipboard.writeText(text);
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2500);
+  };
+
+  const Icon = isCopied ? CheckIcon : ClipboardIcon;
+
+  return (
+    <button
+      disabled={isCopied}
+      onClick={copy}
+      className={clsx(buttonClasses, className)}
+    >
+      <Icon className="mr-1 h-4 w-4" />
+      <span>{isCopied ? "Copied!" : "Copy"}</span>
+    </button>
+  );
+}
--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import * as runtime from "react/jsx-runtime";
 import { Callout } from "./callout";
+import { Pre } from "./pre";
 
 const useMDXComponent = (code: string) => {
   const fn = new Function(code);
@@ -10,6 +11,7 @@ const useMDXComponent = (code: string) => {
 const components = {
   Image,
   Callout,
+  pre: (props: any) => <Pre {...props} raw={props.children} />,
   p: (props: any) => <p {...props} className="text-sm md:text-lg leading-relaxed" />,
   ul: (props: any) => <ul {...props} className="text-sm md:text-lg leading-relaxed" />,
   h1: (props: any) => <h1 {...props} className="text-2xl md:text-4xl font-bold mt-8 mb-4" />,
@@ -24,5 +26,29 @@ interface MdxProps {
 
 export function MDXContent({ code }: MdxProps) {
   const Component = useMDXComponent(code);
-  return <Component components={components} />;
+
+  const extractRawContent = (children: any): string => {
+    if (!children) return ''; // Return an empty string if children is null or undefined
+  
+    if (typeof children === 'string') {
+      // If children is already a string, return it as is
+      return children;
+    } else if (Array.isArray(children)) {
+      // If children is an array, recursively extract raw content from each element
+      return children.map((child: any) => extractRawContent(child)).join('');
+    } else if (typeof children === 'object' && children.props && children.props.children) {
+      // If children is an object with props, extract raw content from its children
+      return extractRawContent(children.props.children);
+    }
+  
+    // If children is of other types, return an empty string
+    return '';
+  };  
+
+  const componentsWithRaw = {
+    ...components,
+    pre: (props: any) => <Pre {...props} raw={extractRawContent(props.children)} />
+  };
+
+  return <Component components={componentsWithRaw} />;
 }
