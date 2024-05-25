72ea6e6c5f4ca8120c2ae696352aac2c8cb805e6
xNewz <pargornruasijan@gmail.com>
2024-05-25 21:16:33 +0700

    fixed: site header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,6 +12,7 @@ export function SiteHeader() {
     <header className="z-10 sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-5xl items-center">
         <MainNav />
+        <MobileNav />
         <div className="flex flex-1 items-center justify-end space-x-2">
           <nav className="flex items-center">
             <Link
@@ -22,7 +23,7 @@ export function SiteHeader() {
               <div
                 className={cn(
                   buttonVariants({ variant: "ghost" }),
-                  "w-10 px-0 hidden sm:inline-flex"
+                  "w-10 px-0 inline-flex"
                 )}
               >
                 <Icons.gitHub className="h-4 w-4" />
@@ -30,7 +31,6 @@ export function SiteHeader() {
               </div>
             </Link>
             <ModeToggle />
-            <MobileNav />
           </nav>
         </div>
       </div>
