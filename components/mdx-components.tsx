12dbcbdf111d0e5303d1cb55264ece4162d3bb46
xNewz <pargornruasijan@gmail.com>
2024-05-26 20:27:43 +0700

    fixed: style mdx component

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -11,12 +11,14 @@ const useMDXComponent = (code: string) => {
 const components = {
   Image,
   Callout,
-  p: (props: any) => <p {...props} className="text-sm md:text-lg leading-relaxed" />,
-  ul: (props: any) => <ul {...props} className="text-sm md:text-lg leading-relaxed" />,
-  h1: (props: any) => <h1 {...props} className="text-2xl md:text-4xl font-bold mt-8 mb-4" />,
-  h2: (props: any) => <h2 {...props} className="text-xl md:text-3xl font-bold mt-8 mb-4" />,
-  h3: (props: any) => <h3 {...props} className="text-lg md:text-2xl font-bold mt-8 mb-4" />,
-  h4: (props: any) => <h4 {...props} className="text-base md:text-xl font-bold mt-8 mb-4" />,
+  p: (props: any) => (
+    <p {...props} className="text-gray-600 dark:text-gray-400 my-4" />
+  ),
+  ul: (props: any) => <ul {...props} className="list-disc pl-6" />,
+  h1: (props: any) => <h1 {...props} className="text-4xl font-bold my-6" />,
+  h2: (props: any) => <h2 {...props} className="text-3xl font-bold my-6" />,
+  h3: (props: any) => <h3 {...props} className="text-2xl font-bold my-6" />,
+  h4: (props: any) => <h4 {...props} className="text-xl font-bold my-6" />,
 };
 
 interface MdxProps {
@@ -27,21 +29,27 @@ export function MDXContent({ code }: MdxProps) {
   const Component = useMDXComponent(code);
 
   const extractRawContent = (children: any): string => {
-    if (!children) return '';
-  
-    if (typeof children === 'string') {
+    if (!children) return "";
+
+    if (typeof children === "string") {
       return children;
     } else if (Array.isArray(children)) {
-      return children.map((child: any) => extractRawContent(child)).join('');
-    } else if (typeof children === 'object' && children.props && children.props.children) {
+      return children.map((child: any) => extractRawContent(child)).join("");
+    } else if (
+      typeof children === "object" &&
+      children.props &&
+      children.props.children
+    ) {
       return extractRawContent(children.props.children);
     }
-    return '';
-  };  
+    return "";
+  };
 
   const componentsWithRaw = {
     ...components,
-    pre: (props: any) => <Pre {...props} raw={extractRawContent(props.children)} />
+    pre: (props: any) => (
+      <Pre {...props} raw={extractRawContent(props.children)} />
+    ),
   };
 
   return <Component components={componentsWithRaw} />;
