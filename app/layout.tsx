6d3dc6df72a91d136134fdce0aa9b72a41cf5eda
xNewz <pargornruasijan@gmail.com>
2024-05-29 02:23:00 +0700

    fixed: scroll-pt-[4rem]

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-pt-[3.5rem]" suppressHydrationWarning>
+    <html lang="en" className="scroll-pt-[4rem]" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
