ca206688292f4364d100d73fb8b9f3cc9c1dcc72
xNewz <pargornruasijan@gmail.com>
2024-06-06 17:31:51 +0700

    add: priority sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -21,26 +21,40 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       {
         url: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
         lastModified: new Date().toISOString(),
+        priority: 1,
+      },
+      {
+        url: `${process.env.NEXT_PUBLIC_SITE_URL}/avatar.webp`,
+        lastModified: new Date().toISOString(),
+      },
+      {
+        url: `${process.env.NEXT_PUBLIC_SITE_URL}/404.png`,
+        lastModified: new Date().toISOString(),
       },
       {
         url: `${process.env.NEXT_PUBLIC_SITE_URL}/about`,
         lastModified: new Date().toISOString(),
+        priority: 0.8,
       },
       {
         url: `${process.env.NEXT_PUBLIC_SITE_URL}/blog`,
         lastModified: new Date().toISOString(),
+        priority: 0.8,
       },
       {
         url: `${process.env.NEXT_PUBLIC_SITE_URL}/tags`,
         lastModified: new Date().toISOString(),
+        priority: 0.6,
       },
       ...postEntries.map((entry: any) => ({
         url: entry.url,
         lastModified: new Date().toISOString(),
+        priority: 0.9,
       })),
       ...tagEntries.map((entry: any) => ({
         url: entry.url,
         lastModified: new Date().toISOString(),
+        priority: 0.7,
       })),
     ];
   } catch (error) {
